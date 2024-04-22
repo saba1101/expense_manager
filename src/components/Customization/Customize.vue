bec40193bde03a0c2cfce001a5607ed3a9c15e25
saba2446 <sabakhara44@gmail.com>
2024-04-22 18:18:05 +0400

    ...

--- src/components/Customization/Customize.vue
+++ src/components/Customization/Customize.vue
@@ -21,44 +21,44 @@ const emit = defineEmits([
     'saved'
 ])
 
-const cardThemes = ref(globalData().card_themes.value.map(el => ({...el,selected:false})))
-const avatars = ref(globalData().user_avatars.value.map(el => ({...el,selected: false})))
+const cardThemes = ref(globalData().card_themes.value.map(el => ({ ...el, selected: false })))
+const avatars = ref(globalData().user_avatars.value.map(el => ({ ...el, selected: false })))
 const username = ref('')
 const error = ref(false)
 
-const setConfig = (key,conf) => {
-    window.localStorage.setItem(key,conf)
+const setConfig = (key, conf) => {
+    window.localStorage.setItem(key, conf)
 }
 
 const getImgSrc = (src) => {
-    let url = new URL('/src/assets/icons/avatars/' + src + '.png',import.meta.url).href
+    let url = new URL('/src/assets/icons/avatars/' + src + '.png', import.meta.url).href
     return url
 }
 
 const select_avatar = (img) => {
     avatars.value.forEach(el => {
-        if(el.key === img.key) el.selected = true
+        if (el.key === img.key) el.selected = true
         else el.selected = false
     })
-    setConfig('userAvatar',img.src)
+    setConfig('userAvatar', img.src)
 }
 
 const select_card_theme = (card) => {
     cardThemes.value.forEach(el => {
-        if(el.key === card.key) el.selected = true
+        if (el.key === card.key) el.selected = true
         else el.selected = false
     })
-    setConfig('userCardTheme',card.key)
+    setConfig('userCardTheme', card.key)
 }
 
 const save_username = () => {
-    if(!username.value || username.value === ''){
+    if (!username.value || username.value === '') {
         error.value = true
         return
     }
-    else{
+    else {
         error.value = false
-        setConfig('userName',username.value)
+        setConfig('userName', username.value)
         emit('saved')
     }
 }
@@ -72,15 +72,17 @@ const save_username = () => {
         <div class="customize-popup">
             <ul class="username">
                 <li>
-                    <input v-model="username" type="text" placeholder="Enter Your Username" :class="[{'error':error}]">
+                    <input v-model="username" type="text" placeholder="Enter Your Username"
+                        :class="[{ 'error': error }]">
                 </li>
             </ul>
             <div class="label">
                 <span>Choose Your Avatar</span>
             </div>
             <ul class="avatar">
-                <li @click="select_avatar(a)" v-for="(a,ind) in avatars" :key="ind" :class="[{'selected':a.selected}]">
-                    <!-- <img :src="getImgSrc(a.src)" alt=""> --> 
+                <li @click="select_avatar(a)" v-for="(a, ind) in avatars" :key="ind"
+                    :class="[{ 'selected': a.selected }]">
+                    <!-- <img :src="getImgSrc(a.src)" alt=""> -->
                     <img v-if="a.src == 1" :src="img1" />
                     <img v-if="a.src == 2" :src="img2" />
                     <img v-if="a.src == 3" :src="img3" />
@@ -93,20 +95,14 @@ const save_username = () => {
             <div class="card_theme_label">
                 <span>Choose Your Card Theme</span>
             </div>
-            <swiper
-                :slides-per-view="1"
-                class="card_slides"
-                :centered-slides="true"
-                :loop="false"
-                :pagination="{ clickable: false }"
-                navigation
-            >
-                <swiper-slide v-for="(t,ind) in cardThemes" :key="ind">
+            <swiper :slides-per-view="1" class="card_slides" :centered-slides="true" :loop="false"
+                :pagination="{ clickable: false }" navigation>
+                <swiper-slide v-for="(t, ind) in cardThemes" :key="ind">
                     <div @click="select_card_theme(t)" :class="['card']">
-                        <Card :theme="t.key" :balance="'8935,00'" style="width: 26.5rem"/>
+                        <Card :theme="t.key" :balance="'8935,00'" style="width: 26.5rem" />
                         <div v-show="t.selected" class="selected-overlay">
                             <img src="@/assets/icons/checkmark.svg" alt="">
-                        </div>                        
+                        </div>
                     </div>
                 </swiper-slide>
             </swiper>
@@ -119,101 +115,103 @@ const save_username = () => {
 </template>
 
 <style lang="scss" scoped>
-.customize-overlay{
+.customize-overlay {
     position: fixed;
     width: 100vw;
     height: 100vh;
     top: 0;
     left: 0;
     z-index: 250;
+    background: rgba(#fff, .3);
     backdrop-filter: blur(3.125rem);
-    mask: linear-gradient(transparent, black 60%);
     @include centerItem;
 
-    .customize-popup{
+    .customize-popup {
         width: 30rem;
         background: #ffffff;
         border-radius: 1.25rem;
         // overflow: auto;
-        border: 0.0625rem solid rgba(#3d3d3d,.1);
+        border: 0.0625rem solid rgba(#3d3d3d, .1);
         padding: 1.25rem;
 
-        .card{
+        .card {
             cursor: grab;
             position: relative;
-            .selected-overlay{
+
+            .selected-overlay {
                 position: absolute;
                 width: 100%;
                 height: 100%;
-                background: rgba(#fff,.3);
+                background: rgba(#fff, .3);
                 top: 0;
                 left: 0;
                 display: grid;
                 place-items: center;
 
-                img{
+                img {
                     width: 3.4375rem;
                 }
             }
         }
 
-        .label{
+        .label {
             margin-top: 1.5625rem;
             margin-left: 0.3125rem;
 
-            span{
+            span {
                 font-size: 1.25rem;
                 color: #3d3d3d;
                 opacity: .7;
             }
         }
 
-        ul{
+        ul {
             width: 100%;
 
-            &.username{
-                li{
+            &.username {
+                li {
                     margin-top: 1.5625rem;
-                    input{
+
+                    input {
                         width: 26.25rem;
                         height: 3rem;
                         padding: 0.625rem;
                         box-sizing: border-box;
                         outline: none;
-                        border: 0.0625rem solid rgba(#3d3d3d,.2);
+                        border: 0.0625rem solid rgba(#3d3d3d, .2);
                         border-radius: 0.375rem;
-                        color: rgba(#3d3d3d,.6);
+                        color: rgba(#3d3d3d, .6);
                         font-size: 0.9375rem;
                         transition: all .2s;
                         background: #ffffff;
 
-                        &.error{
+                        &.error {
                             border: 0.0625rem solid indianred;
                         }
 
-                        &::placeholder{
-                            color: rgba(#3d3d3d,.5);
+                        &::placeholder {
+                            color: rgba(#3d3d3d, .5);
                         }
 
-                        &:focus{
+                        &:focus {
                             border: 0.0625rem solid #FFC248;
                         }
                     }
                 }
             }
 
-            &.avatar{
+            &.avatar {
                 margin-top: 0.9375rem;
                 user-select: none;
-                @include flex(flex-start,center,row);
+                @include flex(flex-start, center, row);
 
-                &:hover{
-                    li{
+                &:hover {
+                    li {
                         opacity: .5;
                     }
                 }
 
-                li{
+                li {
                     width: 2.1875rem;
                     height: 2.1875rem;
                     overflow: hidden;
@@ -224,22 +222,25 @@ const save_username = () => {
                     place-items: center;
                     border-radius: 50%;
 
-                    &.selected{
+                    &.selected {
                         border: 0.3125rem solid lime;
                         transform: scale(1.4);
                     }
 
-                    &:hover{
+                    &:hover {
                         opacity: 1;
                         transform: translateY(-0.3125rem);
-                        &.selected{
+
+                        &.selected {
                             transform: scale(1.4);
                         }
                     }
-                    &:first-child{
+
+                    &:first-child {
                         margin-left: 0;
                     }
-                    img{
+
+                    img {
                         width: 100%;
                         object-fit: contain;
                     }
@@ -249,17 +250,18 @@ const save_username = () => {
 
 
 
-        .card_theme_label{
+        .card_theme_label {
             margin-top: 3.125rem;
             margin-left: 0.3125rem;
-            span{
+
+            span {
                 font-size: 1.25rem;
                 color: #3d3d3d;
                 opacity: .7;
             }
         }
 
-        .card_slides{
+        .card_slides {
             margin-top: 0.9375rem;
         }
 
@@ -267,12 +269,12 @@ const save_username = () => {
 
     }
 
-    .save{
+    .save {
         width: 100%;
         margin-top: 2.1875rem;
         @include centerItem();
-        
-        button{
+
+        button {
             cursor: pointer;
             width: 100%;
             padding: 0.625rem 2.1875rem;
@@ -284,9 +286,11 @@ const save_username = () => {
     }
 }
 
-::v-deep(.card_slides){
-    .swiper-button-prev,.swiper-button-next {
-        color: rgba(#ffffff,.7) !important;
+::v-deep(.card_slides) {
+
+    .swiper-button-prev,
+    .swiper-button-next {
+        color: rgba(#ffffff, .7) !important;
     }
 }
 </style>
